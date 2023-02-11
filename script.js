d1bf85a53b0a502b2d576a8e38ac6d86516d62e4
ishak <120253034+ishakohale@users.noreply.github.com>
2023-02-11 18:35:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,48 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
+
+
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(a => {
+    if (a.marks > 50) {
+     console.log(a);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(a => {
+    if (a.marks > 50) {
+      console.log(a);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newArr = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newArr);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(a => a.marks >= 50);
+  console.log(arr);
+
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [
+    { id: 7, name: "isha", age: "20", marks: 85 },
+    { id: 9, name: "dhanvanti", age: "20", marks: 85 },
+    { id: 11, name: "komal", age: "20", marks: 85 },
+  ];
+  arr = arr.concat(arr1);
+
+console.log(arr);
 }
